f4f8aa85d080ad72a251993b8df65079b994ecd5
Alex <alxmer111@gmail.com>
2018-08-02 16:30:58 +0800

    Clean plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const PATHS = {
     source: path.join(__dirname, 'src'),
@@ -12,7 +14,29 @@ const config = {
         path: PATHS.build + '/js',
         filename: 'index.js'
     },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+
+            }
+        ]
+    },
     plugins: [
+        new HtmlWebpackPlugin({
+            template: PATHS.source + '/index.html',
+            filename: 'index.html',
+            title: 'Webpack App'
+        }),
+        new CleanWebpackPlugin([
+            'dist' 
+        ],
+        {
+             root: __dirname,
+             watch: false 
+        }),
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
